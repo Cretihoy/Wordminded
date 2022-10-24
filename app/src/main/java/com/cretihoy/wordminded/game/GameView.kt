6fd0e3bf326abb586d95ffb5962649863e98a75c
Ellie <ellie@google.com>
2022-10-24 18:11:26 +0200

    added fun show cards

--- app/src/main/java/com/cretihoy/wordminded/game/GameView.kt
+++ app/src/main/java/com/cretihoy/wordminded/game/GameView.kt
@@ -1,4 +1,12 @@
 package com.cretihoy.wordminded.game
 
-interface GameView {
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+import com.cretihoy.wordminded.card.CardModel
+
+interface GameView: MvpView {
+
+    @StateStrategyType(SkipStrategy::class)
+    fun showCards(letters: CardModel, tasks: CardModel)
 }
