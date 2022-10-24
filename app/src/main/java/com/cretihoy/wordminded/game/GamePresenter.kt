7eb03faf9e0073063904baa64286f0c6e2d4f9ef
Ellie <ellie@google.com>
2022-10-24 18:10:21 +0200

    added fun onScreenClicked

--- app/src/main/java/com/cretihoy/wordminded/game/GamePresenter.kt
+++ app/src/main/java/com/cretihoy/wordminded/game/GamePresenter.kt
@@ -1,4 +1,16 @@
 package com.cretihoy.wordminded.game
 
-class GamePresenter {
+import com.arellomobile.mvp.InjectViewState
+import com.arellomobile.mvp.MvpPresenter
+import com.cretihoy.wordminded.card.CardFactory.getRandomLetter
+import com.cretihoy.wordminded.card.CardFactory.getRandomTask
+
+@InjectViewState
+class GamePresenter : MvpPresenter<GameView>() {
+
+    fun onScreenClicked() {
+        val letter = getRandomLetter()
+        val task = getRandomTask()
+        viewState.showCards(letter, task)
+    }
 }
