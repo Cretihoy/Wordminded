cd8adf5e44d1488ed891ec38944a4651ea5f6330
Ellie <ellie@google.com>
2022-10-26 22:27:27 +0200

    changed state strategy

--- app/src/main/java/com/cretihoy/wordminded/game/GameView.kt
+++ app/src/main/java/com/cretihoy/wordminded/game/GameView.kt
@@ -1,12 +1,13 @@
 package com.cretihoy.wordminded.game
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
 import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.cretihoy.wordminded.card.CardModel
 
 interface GameView: MvpView {
 
-    @StateStrategyType(SkipStrategy::class)
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun showCards(letter: CardModel, task: CardModel)
 }
