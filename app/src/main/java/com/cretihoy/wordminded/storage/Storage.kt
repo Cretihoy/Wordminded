7d89c5174960277f285bcadb6fd2a03bcd852a98
ellie <sergzhio238@gmail.com>
2022-11-02 23:30:47 +0100

    create storage

---
+++ app/src/main/java/com/cretihoy/wordminded/storage/Storage.kt
@@ -0,0 +1,13 @@
+package com.cretihoy.wordminded.storage
+
+object Storage {
+    var isHorizontal: Boolean = false
+
+    init {
+        loadSettings()
+    }
+
+    private fun loadSettings() {
+
+    }
+}
