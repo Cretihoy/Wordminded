0858226c207349109a15baf468adb2c0c9bc1219
ellie <sergzhio238@gmail.com>
2022-11-03 17:36:35 +0100

    add save setting orientation

--- app/src/main/java/com/cretihoy/wordminded/storage/Storage.kt
+++ app/src/main/java/com/cretihoy/wordminded/storage/Storage.kt
@@ -1,13 +1,21 @@
 package com.cretihoy.wordminded.storage
 
+import android.app.Activity
+import android.content.Context
+
+private const val PREFERENCES_KEY = "Wordminded"
+private const val IS_HORIZONTAL_KEY = "isHorizontal"
+
 object Storage {
     var isHorizontal: Boolean = false
 
-    init {
-        loadSettings()
+    fun loadSettings(activity: Activity) {
+        val preferences = activity.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE)
+        isHorizontal = preferences.getBoolean(IS_HORIZONTAL_KEY, false)
     }
 
-    private fun loadSettings() {
-
+    fun saveSettings(activity: Activity) {
+        val preferences = activity.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE)
+        preferences.edit().putBoolean(IS_HORIZONTAL_KEY, isHorizontal).apply()
     }
 }
