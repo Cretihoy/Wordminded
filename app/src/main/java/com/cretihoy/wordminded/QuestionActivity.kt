96b9eaca105963fa48d246d6d707f5887b84a895
ellie <sergzhio238@gmail.com>
2022-11-05 22:45:34 +0100

    change button for card view

--- app/src/main/java/com/cretihoy/wordminded/QuestionActivity.kt
+++ app/src/main/java/com/cretihoy/wordminded/QuestionActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.cretihoy.wordminded.game.GameScreenActivity
 
 class QuestionActivity : AppCompatActivity() {
 
-    private val yesButton: Button by lazy { findViewById(R.id.question_yes_button) }
-    private val noButton: Button by lazy { findViewById(R.id.question_no_button) }
+    private val yesButton: CardView by lazy { findViewById(R.id.question_yes_button) }
+    private val noButton: CardView by lazy { findViewById(R.id.question_no_button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
