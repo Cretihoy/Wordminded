b8634858e4f337a8bf846663813e2b8d20356c9b
ellie <sergzhio238@gmail.com>
2022-11-10 19:04:59 +0100

    replace find view by id with data binding in question activity

--- app/src/main/java/com/cretihoy/wordminded/presentation/QuestionActivity.kt
+++ app/src/main/java/com/cretihoy/wordminded/presentation/QuestionActivity.kt
@@ -2,27 +2,29 @@ package com.cretihoy.wordminded.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.cretihoy.wordminded.R
+import com.cretihoy.wordminded.databinding.ActivityQuestionBinding
 import com.cretihoy.wordminded.presentation.base.BaseActivity
 import com.cretihoy.wordminded.presentation.game.GameScreenActivity
 
 class QuestionActivity : BaseActivity() {
 
-    override val rootView: ViewGroup by lazy { findViewById(R.id.root_view) }
-    private val yesButton: Button by lazy { findViewById(R.id.question_yes_button) }
-    private val noButton: Button by lazy { findViewById(R.id.question_no_button) }
+    override val rootView: View by lazy { binding.root }
+    private lateinit var binding: ActivityQuestionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
-        yesButton.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_question)
+
+        binding.questionYesButton.setOnClickListener {
             startActivity(Intent(this, GameScreenActivity::class.java))
             finish()
         }
-        noButton.setOnClickListener {
+        binding.questionNoButton.setOnClickListener {
             finish()
         }
     }
