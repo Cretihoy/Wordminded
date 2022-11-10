35f57ffddb65f7dc8f967831d2d94d83bd5a7343
ellie <sergzhio238@gmail.com>
2022-11-10 18:31:11 +0100

    replace find view by id with data binding

--- app/src/main/java/com/cretihoy/wordminded/presentation/settings/SettingsActivity.kt
+++ app/src/main/java/com/cretihoy/wordminded/presentation/settings/SettingsActivity.kt
@@ -1,31 +1,26 @@
 package com.cretihoy.wordminded.presentation.settings
 
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.widget.SwitchCompat
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.cretihoy.wordminded.R
 import com.cretihoy.wordminded.data.Storage
+import com.cretihoy.wordminded.databinding.ActivitySettingsBinding
 import com.cretihoy.wordminded.extensions.setFontSize
 import com.cretihoy.wordminded.presentation.base.BaseActivity
 
 class SettingsActivity : BaseActivity(), SettingsView {
 
-    override val rootView: ViewGroup by lazy { findViewById(R.id.settings_root) }
-    private val switchButtonHorizontal: SwitchCompat by lazy { findViewById(R.id.switch_button_horizontal) }
-    val buttonMinus: Button by lazy { findViewById(R.id.button_minus_settings) }
-    private val buttonPlus: Button by lazy { findViewById(R.id.button_plus_settings) }
-    val fontTextView: TextView by lazy { findViewById(R.id.font_size_settings) }
+    override val rootView: View by lazy { binding.root }
+    private lateinit var binding: ActivitySettingsBinding
 
     @InjectPresenter
     lateinit var presenter: SettingPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
         initListeners()
     }
 
@@ -35,26 +30,26 @@ class SettingsActivity : BaseActivity(), SettingsView {
     }
 
     private fun initListeners() {
-        switchButtonHorizontal.setOnCheckedChangeListener { _, isChecked ->
+        binding.switchButtonHorizontal.setOnCheckedChangeListener { _, isChecked ->
             presenter.onSwitchChecked(isChecked)
         }
-        buttonMinus.setOnClickListener {
+        binding.buttonMinusSettings.setOnClickListener {
             presenter.onMinusButtonClicked()
         }
-        buttonPlus.setOnClickListener {
+        binding.buttonPlusSettings.setOnClickListener {
             presenter.onPlusButtonClicked()
         }
     }
 
     override fun showFontSize(fontSize: Float) {
-        fontTextView.text = fontSize.toInt().toString()
+        binding.fontSizeSettings.text = fontSize.toInt().toString()
     }
 
     override fun setChecked(horizontal: Boolean) {
-        switchButtonHorizontal.isChecked = horizontal
+        binding.switchButtonHorizontal.isChecked = horizontal
     }
 
     override fun setFontSize(fontSize: Float) {
-        rootView.setFontSize(Storage.fontSize)
+        binding.root.setFontSize(Storage.fontSize)
     }
 }
