f0c8c94d4d3f8cf0f29f95771af8ff37643f44c7
ellie <sergzhio238@gmail.com>
2022-11-10 14:13:48 +0100

    add fun logic for save float size

--- app/src/main/java/com/cretihoy/wordminded/data/Storage.kt
+++ app/src/main/java/com/cretihoy/wordminded/data/Storage.kt
@@ -1,21 +1,25 @@
-package com.cretihoy.wordminded.storage
+package com.cretihoy.wordminded.data
 
 import android.app.Activity
 import android.content.Context
 
 private const val PREFERENCES_KEY = "Wordminded"
 private const val IS_HORIZONTAL_KEY = "isHorizontal"
+private const val FONT_SIZE_KEY = "fontSize"
 
 object Storage {
+    var fontSize: Float = 18f
     var isHorizontal: Boolean = false
 
     fun loadSettings(activity: Activity) {
         val preferences = activity.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE)
         isHorizontal = preferences.getBoolean(IS_HORIZONTAL_KEY, false)
+        fontSize = preferences.getFloat(FONT_SIZE_KEY, 18f)
     }
 
     fun saveSettings(activity: Activity) {
         val preferences = activity.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE)
         preferences.edit().putBoolean(IS_HORIZONTAL_KEY, isHorizontal).apply()
+        preferences.edit().putFloat(FONT_SIZE_KEY, fontSize).apply()
     }
 }
