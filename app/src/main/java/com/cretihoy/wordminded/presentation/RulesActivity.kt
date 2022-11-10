df8c8e0837a2182b0b9360c511bb4fbbc966a6c8
ellie <sergzhio238@gmail.com>
2022-11-10 19:08:13 +0100

    replace find view by id with data binding in rules activity

--- app/src/main/java/com/cretihoy/wordminded/presentation/RulesActivity.kt
+++ app/src/main/java/com/cretihoy/wordminded/presentation/RulesActivity.kt
@@ -1,13 +1,16 @@
 package com.cretihoy.wordminded.presentation
 
 import android.os.Bundle
-import android.view.ViewGroup
+import android.view.View
 import com.cretihoy.wordminded.R
+import com.cretihoy.wordminded.databinding.ActivityRulesBinding
 import com.cretihoy.wordminded.presentation.base.BaseActivity
 
 class RulesActivity : BaseActivity() {
 
-    override val rootView: ViewGroup by lazy { findViewById(R.id.rules_root) }
+    override val rootView: View by lazy { binding.root }
+    private lateinit var binding: ActivityRulesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rules)
