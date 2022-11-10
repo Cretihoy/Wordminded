4f7f489969c5619fb88b7057ccdd4d627c5f2ebb
ellie <sergzhio238@gmail.com>
2022-11-10 18:50:54 +0100

    replace find view by id with data binding in game activity

--- app/src/main/java/com/cretihoy/wordminded/presentation/game/GameActivity.kt
+++ app/src/main/java/com/cretihoy/wordminded/presentation/game/GameActivity.kt
@@ -3,52 +3,47 @@ package com.cretihoy.wordminded.presentation.game
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.cretihoy.wordminded.R
-import com.cretihoy.wordminded.R.layout
 import com.cretihoy.wordminded.data.CardModel
+import com.cretihoy.wordminded.databinding.ActivityGameScreenBinding
 import com.cretihoy.wordminded.presentation.base.BaseActivity
 
 class GameScreenActivity : BaseActivity(), GameView {
 
     override val rootView: ViewGroup by lazy { findViewById(R.id.game_root) }
-    private val taskTop: TextView by lazy { findViewById(R.id.task_top) }
-    private val letterTop: TextView by lazy { findViewById(R.id.letter_top) }
-    private val taskBottom: TextView by lazy { findViewById(R.id.task_bottom) }
-    private val letterBottom: TextView by lazy { findViewById(R.id.letter_bottom) }
-    private val topLinear: LinearLayout by lazy { findViewById(R.id.top_linear) }
-    private val bottomLinear: LinearLayout by lazy { findViewById(R.id.bottom_linear) }
+    private lateinit var binding: ActivityGameScreenBinding
 
     @InjectPresenter
     lateinit var presenter: GamePresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_game_screen)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_game_screen)
 
-        topLinear.setOnClickListener {
+        binding.topLinear.setOnClickListener {
             presenter.onScreenClicked()
         }
-        bottomLinear.setOnClickListener {
+        binding.bottomLinear.setOnClickListener {
             presenter.onScreenClicked()
         }
     }
 
     override fun showCards(letter: CardModel, task: CardModel) {
-        taskBottom.text = getString(task.content)
-        taskTop.text = getString(task.content)
-        letterTop.text = getString(letter.content)
-        letterBottom.text = getString(letter.content)
+        binding.taskBottom.text = getString(task.content)
+        binding.taskTop.text = getString(task.content)
+        binding.letterBottom.text = getString(letter.content)
+        binding.letterTop.text = getString(letter.content)
     }
 
     override fun setOrientation(isHorizontal: Boolean) {
         if (isHorizontal) {
-            topLinear.orientation = LinearLayout.HORIZONTAL
-            bottomLinear.orientation = LinearLayout.HORIZONTAL
+            binding.topLinear.orientation = LinearLayout.HORIZONTAL
+            binding.bottomLinear.orientation = LinearLayout.HORIZONTAL
         } else {
-            topLinear.orientation = LinearLayout.VERTICAL
-            bottomLinear.orientation = LinearLayout.VERTICAL
+            binding.topLinear.orientation = LinearLayout.VERTICAL
+            binding.bottomLinear.orientation = LinearLayout.VERTICAL
         }
     }
 }
