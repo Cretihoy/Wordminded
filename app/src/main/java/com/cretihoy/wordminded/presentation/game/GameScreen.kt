01d11f4c9226eb32b77f78c19c3e4ccd15f11430
ellie <sergzhio238@gmail.com>
2022-11-12 18:09:30 +0100

    create view model in game screen

---
+++ app/src/main/java/com/cretihoy/wordminded/presentation/game/GameScreen.kt
@@ -0,0 +1,12 @@
+package com.cretihoy.wordminded.presentation.game
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun GameScreen(
+    navController: NavHostController,
+    viewModel: GameViewModel
+) {
+
+}
