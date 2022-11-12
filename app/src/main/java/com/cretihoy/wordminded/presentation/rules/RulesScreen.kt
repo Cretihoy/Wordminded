6e8b0fd7a0d64f30d78945b101e4f05539f29502
ellie <sergzhio238@gmail.com>
2022-11-12 18:10:22 +0100

    create view model in rules screen

---
+++ app/src/main/java/com/cretihoy/wordminded/presentation/rules/RulesScreen.kt
@@ -0,0 +1,12 @@
+package com.cretihoy.wordminded.presentation.rules
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun RulesScreen(
+    navController: NavHostController,
+    viewModel: RulesViewModel
+) {
+
+}
---
+++ app/src/main/java/com/cretihoy/wordminded/presentation/rules/RulesViewModel.kt
@@ -0,0 +1,9 @@
+package com.cretihoy.wordminded.presentation.rules
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RulesViewModel
+@Inject constructor() : ViewModel()
