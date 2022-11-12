3f7ed1258c0de26052ea4d59b494160b668a6a2d
ellie <sergzhio238@gmail.com>
2022-11-12 18:10:35 +0100

    create view model in settings screen

---
+++ app/src/main/java/com/cretihoy/wordminded/presentation/settings/SettingsScreen.kt
@@ -0,0 +1,12 @@
+package com.cretihoy.wordminded.presentation.settings
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SettingsScreen(
+    navController: NavHostController,
+    viewModel: SettingsViewModel
+) {
+
+}
