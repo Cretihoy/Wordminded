0fc84636181b5390726918f096f20b33e9a3d556
ellie <sergzhio238@gmail.com>
2022-11-12 18:09:59 +0100

    create view model in menu screen

---
+++ app/src/main/java/com/cretihoy/wordminded/presentation/menu/MenuScreen.kt
@@ -0,0 +1,12 @@
+package com.cretihoy.wordminded.presentation.menu
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun MenuScreen(
+    navController: NavHostController,
+    viewModel: MenuViewModel
+) {
+
+}
---
+++ app/src/main/java/com/cretihoy/wordminded/presentation/menu/MenuViewModel.kt
@@ -0,0 +1,9 @@
+package com.cretihoy.wordminded.presentation.menu
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MenuViewModel
+@Inject constructor() : ViewModel()
