e3e97bc7bdd85cd4e963c7a9c9f271884722ea5e
Ellie <ellie@google.com>
2022-11-13 22:02:06 +0100

    create image view and model

---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/view/ImageModel.kt
@@ -0,0 +1,5 @@
+package com.cretihoy.wordminded.compose.presentation.view
+
+data class ImageModel(
+    val src: Int? = null
+)
---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/view/ImageView.kt
@@ -0,0 +1,33 @@
+package com.cretihoy.wordminded.compose.presentation.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.cretihoy.wordminded.R
+
+@Composable
+fun ImageView(
+    model: ImageModel? = null,
+    modifier: Modifier = Modifier
+) {
+    model?.src?.let {
+        Image(
+            modifier = modifier
+                .fillMaxWidth(),
+            contentDescription = null,
+            painter = painterResource(it)
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewImage() {
+    val model = ImageModel(
+        src = R.drawable.logo
+    )
+    ImageView(model)
+}
