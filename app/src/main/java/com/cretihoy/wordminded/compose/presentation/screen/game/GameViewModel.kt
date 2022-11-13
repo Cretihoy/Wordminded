9892bb2cd169dd4fb9f624ae7074131bb7c18581
Ellie <ellie@google.com>
2022-11-13 13:23:07 +0100

    add game screen  and view model in new package

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameViewModel.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameViewModel.kt
@@ -1,4 +1,4 @@
-package com.cretihoy.wordminded.presentation.game
+package com.cretihoy.wordminded.compose.presentation.screen.game
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
--- app/src/main/java/com/cretihoy/wordminded/presentation/game/GameScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/presentation/game/GameScreen.kt
@@ -2,6 +2,7 @@ package com.cretihoy.wordminded.presentation.game
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import com.cretihoy.wordminded.compose.presentation.screen.game.GameViewModel
 
 @Composable
 fun GameScreen(
