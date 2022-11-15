fd5feb7fd54aeec920252ad6bf67e80f00a589c3
Ellie <ellie@google.com>
2022-11-15 14:18:52 +0100

    add logic in game screen and view model

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameScreen.kt
@@ -1,13 +1,45 @@
 package com.cretihoy.wordminded.compose.presentation.screen.game
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.cretihoy.wordminded.compose.presentation.screen.game.GameViewModel
+import com.cretihoy.wordminded.compose.presentation.view.ButtonView
+import com.cretihoy.wordminded.compose.presentation.view.SpacerView
 
 @Composable
 fun GameScreen(
     navController: NavHostController,
     viewModel: GameViewModel
 ) {
+    ScreenContent(viewModel)
+}
 
+@Composable
+private fun ScreenContent(
+    viewModel: GameViewModel? = null
+) {
+    Column(
+        modifier = Modifier
+            .padding(32.dp)
+            .fillMaxSize()
+    ) {
+        ButtonView(viewModel?.topTask)
+        SpacerView()
+        ButtonView(viewModel?.topLetter)
+        SpacerView()
+        ButtonView(viewModel?.bottomTask)
+        SpacerView()
+        ButtonView(viewModel?.bottomLetter)
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun PreviewScreenContent() {
+    ScreenContent()
 }
--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameViewModel.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameViewModel.kt
@@ -1,9 +1,18 @@
 package com.cretihoy.wordminded.compose.presentation.screen.game
 
 import androidx.lifecycle.ViewModel
+import com.cretihoy.wordminded.compose.presentation.screen.menu.MenuModelFactory
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class GameViewModel
-@Inject constructor() : ViewModel()
+@Inject constructor(
+    private val factory: GameModelFactory
+) : ViewModel() {
+
+    val topTask by lazy { factory.getTaskTopButtonModel() }
+    val topLetter by lazy { factory.getLetterTopButtonModel() }
+    val bottomTask by lazy { factory.getTaskBottomButtonModel() }
+    val bottomLetter by lazy { factory.getLetterBottomButtonModel() }
+}
