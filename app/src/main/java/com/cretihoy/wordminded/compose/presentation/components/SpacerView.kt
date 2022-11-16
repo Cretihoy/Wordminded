f837a832b44cad3a4f4eb677378dc71a93e651f4
Ellie <ellie@google.com>
2022-11-16 21:05:27 +0100

    change logic in spacer view

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/SpacerView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/SpacerView.kt
@@ -1,13 +1,14 @@
-package com.cretihoy.wordminded.compose.presentation.view
+package com.cretihoy.wordminded.compose.presentation.components
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 import com.cretihoy.wordminded.compose.presentation.theme.spacingMedium
 
 @Composable
-fun SpacerView(size: Dp = spacingMedium) {
-    Spacer(modifier = Modifier.size(size))
+fun SpacerView(
+    modifier: Modifier = Modifier
+) {
+    Spacer(modifier.size(spacingMedium))
 }
