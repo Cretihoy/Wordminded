3fbe5666302a76927da689a4a656b5d15c27630c
Ellie <ellie@google.com>
2022-11-19 02:15:14 +0100

    change logic in menu screen

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/menu/MenuScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/menu/MenuScreen.kt
@@ -1,51 +1,26 @@
 package com.cretihoy.wordminded.compose.presentation.screen.menu
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.cretihoy.wordminded.compose.presentation.components.SpacerView
-import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
-import com.cretihoy.wordminded.compose.presentation.components.image.ImageView
-import com.cretihoy.wordminded.extensions.openGameScreen
-import com.cretihoy.wordminded.extensions.openRulesScreen
-import com.cretihoy.wordminded.extensions.openSettingsScreen
+import com.cretihoy.wordminded.compose.presentation.RotateScreen
 
 @Composable
 fun MenuScreen(
     navController: NavHostController,
     viewModel: MenuViewModel
 ) {
-    Column(
-        modifier = Modifier
-            .padding(32.dp)
-            .fillMaxSize()
-    ) {
-        ImageView(
-            model = viewModel.imageModel,
-            modifier = Modifier.weight(1f)
-        )
-        SpacerView()
-        ButtonView(
-            viewModel.startButtonModel,
-            Modifier.fillMaxWidth(),
-            clickAction = { navController.openGameScreen() }
-        )
-        SpacerView()
-        ButtonView(
-            viewModel.rulesButtonModel,
-            Modifier.fillMaxWidth(),
-            clickAction = { navController.openRulesScreen() }
-        )
-        SpacerView()
-        ButtonView(
-            viewModel.settingsButtonModel,
-            Modifier.fillMaxWidth(),
-            clickAction = { navController.openSettingsScreen() }
-        )
-    }
+    RotateScreen(
+        horizontal = {
+            MenuHorizontalScreen(
+                navController = navController,
+                viewModel = viewModel
+            )
+        },
+        vertical = {
+            MenuVerticalScreen(
+                navController = navController,
+                viewModel = viewModel
+            )
+        }
+    )
 }
