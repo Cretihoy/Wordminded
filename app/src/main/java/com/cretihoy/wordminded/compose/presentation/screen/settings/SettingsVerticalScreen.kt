4c2c9a2937657098e4f78739843cfd056451add7
Ellie <ellie@google.com>
2022-11-20 15:30:37 +0100

    add logic in settings vertical screen

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsVerticalScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsVerticalScreen.kt
@@ -1,11 +1,26 @@
 package com.cretihoy.wordminded.compose.presentation.screen.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import com.cretihoy.wordminded.compose.presentation.screen.game.GameViewModel
+import com.cretihoy.wordminded.compose.presentation.components.counter.CounterView
+import com.cretihoy.wordminded.compose.presentation.components.switch.SwitchView
+import com.cretihoy.wordminded.compose.presentation.components.text.TextView
+import com.cretihoy.wordminded.compose.presentation.theme.spacingLarge
 
 @Composable
 fun SettingsVerticalScreen(
     navController: NavHostController,
     viewModel: SettingsViewModel
-) {}
+) {
+    Column(
+        Modifier
+            .padding(spacingLarge)
+    ) {
+        TextView(viewModel.settingsTitle, Modifier.weight(1f))
+        SwitchView(viewModel.orientationSettingsSwitch)
+        CounterView(viewModel.counterModel)
+    }
+}
