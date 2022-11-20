8ca6122c40bc8fb7dfd85632aba95e0cc989a153
Ellie <ellie@google.com>
2022-11-20 14:50:20 +0100

    create game vertical and horizontal screen

---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameHorizontalScreen.kt
@@ -0,0 +1,11 @@
+package com.cretihoy.wordminded.compose.presentation.screen.game
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.cretihoy.wordminded.compose.presentation.screen.menu.MenuViewModel
+
+@Composable
+fun GameHorizontalScreen(
+    navController: NavHostController,
+    viewModel: GameViewModel
+) {}
