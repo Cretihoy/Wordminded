3791152ab9a7ab6488f4f16ab2c64dd29234c87d
Ellie <ellie@google.com>
2022-11-20 15:17:16 +0100

    create settings horizontal and vertical screen

---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsHorizontalScreen.kt
@@ -0,0 +1,11 @@
+package com.cretihoy.wordminded.compose.presentation.screen.settings
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.cretihoy.wordminded.compose.presentation.screen.game.GameViewModel
+
+@Composable
+fun SettingsHorizontalScreen(
+    navController: NavHostController,
+    viewModel: SettingsViewModel
+) {}
---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsVerticalScreen.kt
@@ -0,0 +1,11 @@
+package com.cretihoy.wordminded.compose.presentation.screen.settings
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.cretihoy.wordminded.compose.presentation.screen.game.GameViewModel
+
+@Composable
+fun SettingsVerticalScreen(
+    navController: NavHostController,
+    viewModel: SettingsViewModel
+) {}
