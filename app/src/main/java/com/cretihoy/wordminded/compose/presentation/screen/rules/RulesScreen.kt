5f7d396ed2e648dbcc72501a9fde2ff26672d50d
Ellie <ellie@google.com>
2022-11-20 14:40:37 +0100

    change logic in rules screen

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/rules/RulesScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/rules/RulesScreen.kt
@@ -1,40 +1,26 @@
 package com.cretihoy.wordminded.compose.presentation.screen.rules
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.cretihoy.wordminded.compose.presentation.components.SpacerView
-import com.cretihoy.wordminded.compose.presentation.components.text.TextView
-import com.cretihoy.wordminded.compose.presentation.components.textBlock.TextBlockView
+import com.cretihoy.wordminded.compose.presentation.RotateScreen
 
 @Composable
 fun RulesScreen(
     navController: NavHostController,
     viewModel: RulesViewModel
 ) {
-    ScreenContent(viewModel)
-}
-
-@Composable
-private fun ScreenContent(
-    viewModel: RulesViewModel? = null
-) {
-    Column(
-        modifier = Modifier
-            .padding(32.dp)
-            .fillMaxSize()
-    ) {
-        TextView(viewModel?.rulesTitle, Modifier.weight(1f))
-        SpacerView()
-        TextBlockView(viewModel?.rulesOne)
-        SpacerView()
-        TextBlockView(viewModel?.rulesTwo)
-        SpacerView()
-        TextBlockView(viewModel?.rulesThree)
-    }
+    RotateScreen(
+        horizontal = {
+            RulesHorizontalScreen(
+                navController = navController,
+                viewModel = viewModel
+            )
+        },
+        vertical = {
+            RulesVerticalScreen(
+                navController = navController,
+                viewModel = viewModel
+            )
+        }
+    )
 }
