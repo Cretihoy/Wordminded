e0f79d5a82b53e9f95a5da7c7c292c5adbb878e5
Ellie <ellie@google.com>
2022-11-20 15:30:49 +0100

    add logic in settings screen

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsScreen.kt
@@ -1,34 +1,26 @@
 package com.cretihoy.wordminded.compose.presentation.screen.settings
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import com.cretihoy.wordminded.compose.presentation.components.counter.CounterView
-import com.cretihoy.wordminded.compose.presentation.components.switch.SwitchView
-import com.cretihoy.wordminded.compose.presentation.components.text.TextView
-import com.cretihoy.wordminded.compose.presentation.theme.spacingLarge
+import com.cretihoy.wordminded.compose.presentation.RotateScreen
 
 @Composable
 fun SettingsScreen(
     navController: NavHostController,
     viewModel: SettingsViewModel
 ) {
-    ScreenContent(viewModel)
-}
-
-@Composable
-private fun ScreenContent(
-    viewModel: SettingsViewModel? = null
-) {
-    Column(
-        Modifier
-            .padding(spacingLarge)
-    ) {
-        TextView(viewModel?.settingsTitle, Modifier.weight(1f))
-        SwitchView(viewModel?.orientationSettingsSwitch)
-        CounterView(viewModel?.counterModel)
-    }
+    RotateScreen(
+        horizontal = {
+            SettingsHorizontalScreen(
+                navController = navController,
+                viewModel = viewModel
+            )
+        },
+        vertical = {
+            SettingsVerticalScreen(
+                navController = navController,
+                viewModel = viewModel
+            )
+        }
+    )
 }
