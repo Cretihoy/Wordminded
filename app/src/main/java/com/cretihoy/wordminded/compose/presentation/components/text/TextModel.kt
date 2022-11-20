66e18fef8721aaca5b5f2a3f31715504b39cbd78
Ellie <ellie@google.com>
2022-11-20 19:57:27 +0100

    add font size value to text model

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/text/TextModel.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/text/TextModel.kt
@@ -1,6 +1,10 @@
 package com.cretihoy.wordminded.compose.presentation.components.text
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
 data class TextModel(
+    val fontSize: MutableState<Float> = mutableStateOf(18f),
     val textAttr: Int? = null,
     val isTitle: Boolean = false
 )
