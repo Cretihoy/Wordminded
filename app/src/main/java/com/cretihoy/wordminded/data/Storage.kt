1d5cf96fdb62ce0d346f4ec5963079bcdd254874
Ellie <ellie@google.com>
2022-11-20 19:57:10 +0100

    make storage class

--- app/src/main/java/com/cretihoy/wordminded/data/Storage.kt
+++ app/src/main/java/com/cretihoy/wordminded/data/Storage.kt
@@ -2,24 +2,31 @@ package com.cretihoy.wordminded.data
 
 import android.app.Activity
 import android.content.Context
+import androidx.compose.runtime.mutableStateOf
+import javax.inject.Inject
 
 private const val PREFERENCES_KEY = "Wordminded"
 private const val IS_HORIZONTAL_KEY = "isHorizontal"
 private const val FONT_SIZE_KEY = "fontSize"
 
-object Storage {
-    var fontSize: Float = 18f
-    var isHorizontal: Boolean = false
+class Storage
+@Inject constructor(
+    private val activity: Activity
+) {
 
-    fun loadSettings(activity: Activity) {
+    var fontSize = mutableStateOf(18f)
+    var isHorizontal = mutableStateOf(false)
+
+    // todo: activity: Activity here if its not working
+    fun loadSettings() {
         val preferences = activity.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE)
-        isHorizontal = preferences.getBoolean(IS_HORIZONTAL_KEY, false)
-        fontSize = preferences.getFloat(FONT_SIZE_KEY, 18f)
+        isHorizontal.value = preferences.getBoolean(IS_HORIZONTAL_KEY, false)
+        fontSize.value = preferences.getFloat(FONT_SIZE_KEY, 18f)
     }
 
     fun saveSettings(activity: Activity) {
         val preferences = activity.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE)
-        preferences.edit().putBoolean(IS_HORIZONTAL_KEY, isHorizontal).apply()
-        preferences.edit().putFloat(FONT_SIZE_KEY, fontSize).apply()
+        preferences.edit().putBoolean(IS_HORIZONTAL_KEY, isHorizontal.value).apply()
+        preferences.edit().putFloat(FONT_SIZE_KEY, fontSize.value).apply()
     }
 }
