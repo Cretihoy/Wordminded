327c5f7e54fd087b6bdf04087797356a0f1e76b2
ellie <sergzhio238@gmail.com>
2022-11-27 00:12:49 +0100

    add timer flag to menu vm

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/menu/MenuViewModel.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/menu/MenuViewModel.kt
@@ -1,5 +1,6 @@
 package com.cretihoy.wordminded.compose.presentation.screen.menu
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,6 +11,7 @@ class MenuViewModel
     private val factory: MenuModelFactory
 ) : ViewModel() {
 
+    val isShown = mutableStateOf(false)
     val imageModel by lazy { factory.getImageModel() }
     val startButtonModel by lazy { factory.getStartButtonModel() }
     val rulesButtonModel by lazy { factory.getRulesButtonModel() }
