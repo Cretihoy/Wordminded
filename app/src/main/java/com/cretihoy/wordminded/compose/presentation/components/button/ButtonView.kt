86f7f4a05446c08ea65de009eda99f0cc463e98a
ellie <sergzhio238@gmail.com>
2022-11-27 14:58:21 +0100

    add text align

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/button/ButtonView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/button/ButtonView.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,7 +45,8 @@ fun ButtonView(
                     modifier = Modifier.padding(spacingSmall),
                     text = stringResource(id = text),
                     color = colors.onSecondary,
-                    fontSize = model.fontSize.value.sp
+                    fontSize = model.fontSize.value.sp,
+                    textAlign = TextAlign.Center,
                 )
             }
         }
