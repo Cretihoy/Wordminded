019273d95c42018aeba30578c7496c5a74b0c12a
ellie <sergzhio238@gmail.com>
2022-11-28 21:29:45 +0100

    add infinity game in vertical and horizontal game screen

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameVerticalScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameVerticalScreen.kt
@@ -1,11 +1,14 @@
 package com.cretihoy.wordminded.compose.presentation.screen.game
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.cretihoy.wordminded.compose.presentation.components.SpacerView
 import com.cretihoy.wordminded.compose.presentation.components.gameside.GameSideView
@@ -19,17 +22,26 @@ fun GameVerticalScreen(
             .padding(32.dp)
             .fillMaxSize()
     ) {
-        GameSideView(viewModel.gameSide.value,
-            Modifier
-                .weight(1f)
-                .rotate(180f),
-            actionLetter = { viewModel.onClick() },
-            actionTask = { viewModel.onClick() }
-        )
-        SpacerView()
-        GameSideView(viewModel.gameSide.value, Modifier.weight(1f),
-            actionLetter = { viewModel.onClick() },
-            actionTask = { viewModel.onClick() }
-        )
+        if (viewModel?.gameSide?.value?.task?.textAttr?.value != null && viewModel?.gameSide?.value?.letter?.textAttr?.value != null) {
+            GameSideView(viewModel.gameSide.value,
+                Modifier
+                    .weight(1f)
+                    .rotate(180f),
+                actionLetter = { viewModel.onClick() },
+                actionTask = { viewModel.onClick() }
+            )
+            SpacerView()
+            GameSideView(viewModel.gameSide.value, Modifier.weight(1f),
+                actionLetter = { viewModel.onClick() },
+                actionTask = { viewModel.onClick() }
+            )
+        } else {
+            Log.d("asdasd", "NUL")
+            Text(
+                text = "Ну всё, пиздец, поиграли блеать.",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
     }
 }
