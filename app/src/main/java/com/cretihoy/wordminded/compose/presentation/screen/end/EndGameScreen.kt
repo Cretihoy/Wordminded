d51a3386a2d30b55babb79716fd2efdf009fa16c
ellie <sergzhio238@gmail.com>
2022-11-29 14:32:31 +0100

    add logic in end game screen

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/end/EndGameScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/end/EndGameScreen.kt
@@ -1,2 +1,26 @@
 package com.cretihoy.wordminded.compose.presentation.screen.end
 
+import androidx.compose.runtime.Composable
+import com.cretihoy.wordminded.compose.presentation.components.RotateScreen
+import com.cretihoy.wordminded.compose.presentation.screen.game.EndGameModel
+
+@Composable
+fun EndGameScreen(
+    model: EndGameModel,
+    clickAction: () -> Unit = {}
+) {
+    RotateScreen(
+        horizontal = {
+            EndGameHorizontalScreen(
+                model = model,
+                clickAction = clickAction
+            )
+        },
+        vertical = {
+            EndGameVerticalScreen(
+                model = model,
+                clickAction = clickAction
+            )
+        }
+    )
+}
