155ef4734cc6d63018944b36eed95c0b787f6e91
ellie <sergzhio238@gmail.com>
2022-11-29 14:32:07 +0100

    add logic in end game horizontal and vertical screen

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/end/EndGameHorizontalScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/end/EndGameHorizontalScreen.kt
@@ -1,2 +1,39 @@
 package com.cretihoy.wordminded.compose.presentation.screen.end
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cretihoy.wordminded.compose.presentation.components.SpacerView
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
+import com.cretihoy.wordminded.compose.presentation.components.text.TextView
+import com.cretihoy.wordminded.compose.presentation.screen.game.EndGameModel
+
+@Composable
+fun EndGameHorizontalScreen(
+    model: EndGameModel,
+    clickAction: () -> Unit = {}
+) {
+    Row(
+        modifier = Modifier
+            .padding(32.dp)
+            .fillMaxSize()
+    ) {
+        TextView(
+            model.title,
+            Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        )
+        SpacerView()
+        ButtonView(
+            model = model.button,
+            modifier = Modifier.fillMaxWidth(),
+            clickAction = clickAction
+        )
+    }
+}
--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/end/EndGameVerticalScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/end/EndGameVerticalScreen.kt
@@ -1,2 +1,39 @@
 package com.cretihoy.wordminded.compose.presentation.screen.end
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cretihoy.wordminded.compose.presentation.components.SpacerView
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
+import com.cretihoy.wordminded.compose.presentation.components.text.TextView
+import com.cretihoy.wordminded.compose.presentation.screen.game.EndGameModel
+
+@Composable
+fun EndGameVerticalScreen(
+    model: EndGameModel,
+    clickAction: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .padding(32.dp)
+            .fillMaxSize()
+    ) {
+        TextView(
+            model.title,
+            Modifier
+                .weight(1f)
+                .align(Alignment.CenterHorizontally)
+        )
+        SpacerView()
+        ButtonView(
+            model = model.button,
+            modifier = Modifier.fillMaxWidth(),
+            clickAction = clickAction
+        )
+    }
+}
