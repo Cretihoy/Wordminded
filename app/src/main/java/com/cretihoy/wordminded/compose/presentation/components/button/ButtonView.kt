dbb2dbd1c66352577f41d9a2fe7e47099a5b56e2
ellie <sergzhio238@gmail.com>
2022-11-29 12:38:29 +0100

    delete text align

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/button/ButtonView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/button/ButtonView.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,7 +45,6 @@ fun ButtonView(
                     text = stringResource(id = text),
                     color = colors.onSecondary,
                     fontSize = model.fontSize.value.sp,
-                    textAlign = TextAlign.Center,
                 )
             }
         }
