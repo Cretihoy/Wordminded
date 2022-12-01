bff276285f6e6ff3d8d5cc45d118e1932f081079
ellie <sergzhio238@gmail.com>
2022-12-01 14:26:02 +0100

    add dialog in settings screens

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsHorizontalScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsHorizontalScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import com.cretihoy.wordminded.compose.presentation.components.SpacerView
 import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
 import com.cretihoy.wordminded.compose.presentation.components.counter.CounterView
+import com.cretihoy.wordminded.compose.presentation.components.dialog.DialogView
 import com.cretihoy.wordminded.compose.presentation.components.switch.SwitchView
 import com.cretihoy.wordminded.compose.presentation.components.text.TextView
 import com.cretihoy.wordminded.compose.presentation.theme.spacingLarge
@@ -17,38 +18,48 @@ import com.cretihoy.wordminded.compose.presentation.theme.spacingLarge
 fun SettingsHorizontalScreen(
     viewModel: SettingsViewModel
 ) {
-    Row(
-        modifier = Modifier
-            .padding(end = spacingLarge)
-            .fillMaxSize()
-    ) {
-        TextView(
-            viewModel.settingsTitle,
+    Box {
+        Row(
             Modifier
-                .weight(1f)
-                .align(CenterVertically)
-        )
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .align(CenterVertically)
                 .padding(end = spacingLarge)
-                .verticalScroll(rememberScrollState())
         ) {
-            SwitchView(viewModel.orientationSettingsSwitch)
-            SpacerView()
-            SwitchView(viewModel.themeSettingsSwitch)
-            SpacerView()
-            SwitchView(viewModel.infinityGameSwitch)
-            SpacerView()
-            CounterView(viewModel.counterModel)
-            SpacerView()
-            ButtonView(viewModel.resetSettings,
-                Modifier.fillMaxWidth(),
-                clickAction = {
-                    viewModel.onResetSettingsClicked()
-                })
-            SpacerView(Modifier.size(spacingLarge))
+            TextView(
+                viewModel.settingsTitle,
+                Modifier
+                    .weight(1f)
+                    .align(CenterVertically)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .align(CenterVertically)
+                    .padding(end = spacingLarge)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                SwitchView(viewModel.orientationSettingsSwitch)
+                SpacerView()
+                SwitchView(viewModel.themeSettingsSwitch)
+                SpacerView()
+                SwitchView(viewModel.infinityGameSwitch)
+                SpacerView()
+                CounterView(viewModel.counterModel)
+                SpacerView()
+                ButtonView(viewModel.resetSettingsButtonModel,
+                    Modifier.fillMaxWidth(),
+                    clickAction = {
+                        viewModel.isShown.value = true
+                    })
+            }
         }
+        DialogView(
+            viewModel.isShown,
+            model = viewModel.dialog,
+            leftClick = {
+                viewModel.isShown.value = false
+            }, rightClick = {
+                viewModel.onResetSettingsClicked()
+                viewModel.isShown.value = false
+            }
+        )
     }
 }
--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsVerticalScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/settings/SettingsVerticalScreen.kt
@@ -1,14 +1,16 @@
 package com.cretihoy.wordminded.compose.presentation.screen.settings
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import com.cretihoy.wordminded.compose.presentation.components.SpacerView
 import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
 import com.cretihoy.wordminded.compose.presentation.components.counter.CounterView
+import com.cretihoy.wordminded.compose.presentation.components.dialog.DialogView
 import com.cretihoy.wordminded.compose.presentation.components.switch.SwitchView
 import com.cretihoy.wordminded.compose.presentation.components.text.TextView
 import com.cretihoy.wordminded.compose.presentation.theme.spacingLarge
@@ -17,28 +19,40 @@ import com.cretihoy.wordminded.compose.presentation.theme.spacingLarge
 fun SettingsVerticalScreen(
     viewModel: SettingsViewModel
 ) {
-    Column(
-        Modifier
-            .padding(spacingLarge)
-    ) {
-        TextView(
-            viewModel.settingsTitle,
+    Box {
+        Column(
             Modifier
-                .weight(1f)
-                .align(Alignment.CenterHorizontally)
-        )
-        SwitchView(viewModel.orientationSettingsSwitch)
-        SpacerView()
-        SwitchView(viewModel.themeSettingsSwitch)
-        SpacerView()
-        SwitchView(viewModel.infinityGameSwitch)
-        SpacerView()
-        CounterView(viewModel.counterModel)
-        SpacerView()
-        ButtonView(viewModel.resetSettings,
-            Modifier.fillMaxWidth(),
-            clickAction = {
+                .padding(spacingLarge)
+        ) {
+            TextView(
+                viewModel.settingsTitle,
+                Modifier
+                    .weight(1f)
+                    .align(CenterHorizontally)
+            )
+            SwitchView(viewModel.orientationSettingsSwitch)
+            SpacerView()
+            SwitchView(viewModel.themeSettingsSwitch)
+            SpacerView()
+            SwitchView(viewModel.infinityGameSwitch)
+            SpacerView()
+            CounterView(viewModel.counterModel)
+            SpacerView()
+            ButtonView(viewModel.resetSettingsButtonModel,
+                Modifier.fillMaxWidth(),
+                clickAction = {
+                    viewModel.isShown.value = true
+                })
+        }
+        DialogView(
+            viewModel.isShown,
+            model = viewModel.dialog,
+            leftClick = {
+                viewModel.isShown.value = false
+            }, rightClick = {
                 viewModel.onResetSettingsClicked()
-            })
+                viewModel.isShown.value = false
+            }
+        )
     }
 }
