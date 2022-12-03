5c65526f0ea2a84f810d6c5314402d5f5bd03b8a
Alexander Vortex <avorteks@alfabank.ru>
2022-12-03 23:35:02 +0300

    create modal dailog view

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/modal/ModalView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/modal/ModalView.kt
@@ -1,4 +1,4 @@
-package com.cretihoy.wordminded.compose.presentation.components.wrapper
+package com.cretihoy.wordminded.compose.presentation.components.modal
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -6,8 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -15,32 +14,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.cretihoy.wordminded.compose.presentation.components.SpacerView
-import com.cretihoy.wordminded.compose.presentation.components.button.ButtonModel
-import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
 import com.cretihoy.wordminded.compose.presentation.theme.Shapes
 import com.cretihoy.wordminded.compose.presentation.theme.spacingMedium
 
 @Composable
-fun NewDialogView(
+fun ModalView(
     isShown: MutableState<Boolean>,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
     if (isShown.value) {
         Box(
-            contentAlignment = Alignment.Center,
-            modifier = modifier
-                .clickable { isShown.value = false }
-                .fillMaxSize()
-                .alpha(0.1f)
-                .background(MaterialTheme.colors.onBackground)) {
-            Column(
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
                 modifier = Modifier
+                    .clickable { isShown.value = false }
+                    .fillMaxSize()
+                    .alpha(0.1f)
+                    .background(colors.onBackground))
+            Column(
+                modifier = modifier
                     .padding(spacingMedium)
                     .clip(Shapes.large)
-                    .background(MaterialTheme.colors.background)
+                    .background(colors.background)
                     .padding(spacingMedium)
             ) {
                 content.invoke()
@@ -51,36 +50,14 @@ fun NewDialogView(
 
 @Preview
 @Composable
-fun PreviewDialogView() {
-    val model = ButtonModel(
-        fontSize = mutableStateOf(18f),
-        text = "Button"
-    )
-    NewDialogView(mutableStateOf(true)) {
-        Column {
-            Text(text = "BLAHBLAH")
-            SpacerView()
-            Text(text = "This is text")
-            SpacerView()
-            ButtonView(model)
-        }
-    }
+fun PreviewModalView() {
+    ModalView(
+        mutableStateOf(true),
+        content = {
+            Box(
+                modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxSize()
+            )
+        })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
