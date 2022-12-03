6057e0f267039291df9aac9fcc4a83a324db0855
Alexander Vortex <avorteks@alfabank.ru>
2022-12-03 16:44:30 +0300

    add model & factory

---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/user/UserModel.kt
@@ -0,0 +1,9 @@
+package com.cretihoy.wordminded.compose.presentation.components.user
+
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonModel
+
+data class UserModel(
+    val nameButton: ButtonModel,
+    val editButton: ButtonModel,
+    val removeButton: ButtonModel
+)
---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/users/UserModelFactory.kt
@@ -0,0 +1,35 @@
+package com.cretihoy.wordminded.compose.presentation.screen.users
+
+import androidx.compose.runtime.mutableStateOf
+import com.cretihoy.wordminded.R
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonModel
+import com.cretihoy.wordminded.compose.presentation.components.image.ImageModel
+import com.cretihoy.wordminded.compose.presentation.components.text.TextModel
+import com.cretihoy.wordminded.data.Storage
+import javax.inject.Inject
+
+class UserModelFactory @Inject constructor(
+    private val storage: Storage
+) {
+
+    fun getNewUserButtonModel(): ButtonModel {
+        return ButtonModel(
+            fontSize = storage.fontSize,
+            textAttr = mutableStateOf(R.string.plus)
+        )
+    }
+
+    fun getTitleModel(): TextModel {
+        return TextModel(
+            fontSize = storage.fontSize,
+            textAttr = R.string.user_management,
+            isTitle = true,
+            image = ImageModel(
+                src = R.drawable.logo,
+                srcAlt = R.drawable.logo_inverted,
+                isInverted = storage.isAltTheme
+            )
+        )
+    }
+
+}
