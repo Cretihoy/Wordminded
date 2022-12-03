b3893ad7288f9c73c1562b718ae78710d2ac8171
Alexander Vortex <avorteks@alfabank.ru>
2022-12-03 23:35:02 +0300

    create modal dailog view

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/dialog/DialogView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/dialog/DialogView.kt
@@ -1,16 +1,12 @@
 package com.cretihoy.wordminded.compose.presentation.components.dialog
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import com.cretihoy.wordminded.R
 import com.cretihoy.wordminded.compose.presentation.components.SpacerView
@@ -18,9 +14,8 @@ import com.cretihoy.wordminded.compose.presentation.components.button.ButtonMode
 import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
 import com.cretihoy.wordminded.compose.presentation.components.text.TextModel
 import com.cretihoy.wordminded.compose.presentation.components.text.TextView
-import com.cretihoy.wordminded.compose.presentation.theme.Shapes
+import com.cretihoy.wordminded.compose.presentation.components.modal.ModalView
 import com.cretihoy.wordminded.compose.presentation.theme.minWidth
-import com.cretihoy.wordminded.compose.presentation.theme.spacingMedium
 
 @Composable
 fun DialogView(
@@ -30,47 +25,27 @@ fun DialogView(
     leftClick: () -> Unit = {},
     rightClick: () -> Unit = {}
 ) {
-    if (isShown.value) {
-        model?.let {
-            Box(
-                contentAlignment = Alignment.Center
-            ) {
-                Box(
-                    Modifier
-                        .clickable {
-                            isShown.value = false
-                        }
-                        .fillMaxSize()
-                        .alpha(0.1f)
-                        .background(colors.onBackground))
-                Column(
-                    modifier = modifier
-                        .padding(spacingMedium)
-                        .clip(Shapes.large)
-                        .background(colors.background)
-                        .padding(spacingMedium)
-                ) {
-                    TextView(
-                        model = model.title,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
-                    SpacerView(model.title, model.description)
-                    TextView(model.description)
-                    SpacerView()
-                    Row {
-                        ButtonView(
-                            model.leftButton,
-                            clickAction = leftClick,
-                            modifier = Modifier.sizeIn(minWidth = minWidth)
-                        )
-                        SpacerView(model.leftButton, model.rightButton)
-                        ButtonView(
-                            model.rightButton,
-                            clickAction = rightClick,
-                            modifier = Modifier.sizeIn(minWidth = minWidth)
-                        )
-                    }
-                }
+    model?.let {
+        ModalView(
+            isShown = isShown,
+            modifier = modifier
+        ) {
+            TextView(model.title, Modifier.fillMaxWidth())
+            SpacerView(model.title, model.description)
+            TextView(model.description)
+            SpacerView()
+            Row {
+                ButtonView(
+                    model.leftButton,
+                    clickAction = leftClick,
+                    modifier = Modifier.sizeIn(minWidth = minWidth)
+                )
+                SpacerView(model.leftButton, model.rightButton)
+                ButtonView(
+                    model.rightButton,
+                    clickAction = rightClick,
+                    modifier = Modifier.sizeIn(minWidth = minWidth)
+                )
             }
         }
     }
