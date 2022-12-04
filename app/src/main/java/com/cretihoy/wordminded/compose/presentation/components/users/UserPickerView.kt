a818f01c8ee8843ad08cbbde35e2bcec1a5e0472
Alexander Vortex <avorteks@alfabank.ru>
2022-12-04 23:55:41 +0300

    add button

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/users/UserPickerView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/users/UserPickerView.kt
@@ -1,8 +1,10 @@
 package com.cretihoy.wordminded.compose.presentation.components.users
 
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
 import com.cretihoy.wordminded.compose.presentation.components.modal.ModalView
 import com.cretihoy.wordminded.compose.presentation.components.text.TextView
 
@@ -13,5 +15,6 @@ fun UserPickerView(
     val viewModel = hiltViewModel<UserPickerViewModel>()
     ModalView(isShown = isShown) {
         TextView(viewModel.titleModel)
+        ButtonView(viewModel.nextButtonModel)
     }
 }
--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/users/UserPickerViewModel.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/users/UserPickerViewModel.kt
@@ -1,6 +1,7 @@
 package com.cretihoy.wordminded.compose.presentation.components.users
 
 import androidx.lifecycle.ViewModel
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonModel
 import com.cretihoy.wordminded.compose.presentation.screen.users.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -12,5 +13,6 @@ class UserPickerViewModel
     private val factory: UserPickerFactory
 ) : ViewModel() {
 
+    val nextButtonModel by lazy { factory.getNextButtonModel() }
     val titleModel by lazy { factory.getTitleModel() }
 }
