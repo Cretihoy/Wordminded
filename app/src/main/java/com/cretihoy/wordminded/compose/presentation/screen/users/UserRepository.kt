68c04bb9480c3652703e5897e65410daf1f270a6
ellie <sergzhio238@gmail.com>
2022-12-05 14:34:08 +0100

    create and add logic in user repository

---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/users/UserRepository.kt
@@ -0,0 +1,25 @@
+package com.cretihoy.wordminded.compose.presentation.screen.users
+
+import com.cretihoy.wordminded.compose.presentation.components.user.UserModel
+import com.cretihoy.wordminded.data.db.dao.UserDao
+import javax.inject.Inject
+
+class UserRepository @Inject constructor(
+    private val database: UserDao,
+    private val mapper: UserMapper
+) {
+
+    suspend fun getUsers(): List<UserModel> {
+        return database.getAll().map { mapper.map(it) }
+    }
+
+    suspend fun addUser(model: UserModel) {
+        val user = mapper.map(model)
+        database.add(user)
+    }
+
+    suspend fun removeUser(model: UserModel) {
+        val user = mapper.map(model)
+        database.remove(user)
+    }
+}
