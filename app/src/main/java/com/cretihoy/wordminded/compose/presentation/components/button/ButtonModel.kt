0f6017500cb5115408cbb0a16321c4c09dc7a41c
ellie <sergzhio238@gmail.com>
2022-12-05 14:32:59 +0100

    add icon in button model and view

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/button/ButtonModel.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/button/ButtonModel.kt
@@ -2,9 +2,12 @@ package com.cretihoy.wordminded.compose.presentation.components.button
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.vector.ImageVector
 
 data class ButtonModel(
+    val icon: ImageVector? = null,
     val fontSize: MutableState<Float>,
+    val text: String? = null,
     val textAttr: MutableState<Int?> = mutableStateOf(null),
     val isSecondary: Boolean = false
 )
--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/button/ButtonView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/button/ButtonView.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
@@ -15,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cretihoy.wordminded.R
+import com.cretihoy.wordminded.compose.presentation.theme.EMPTY_STRING
 import com.cretihoy.wordminded.compose.presentation.theme.Shapes
 import com.cretihoy.wordminded.compose.presentation.theme.spacingSmall
 
@@ -24,13 +28,13 @@ fun ButtonView(
     modifier: Modifier = Modifier,
     clickAction: () -> Unit = {}
 ) {
-    model?.let {
+    model?.let { it ->
         Button(
             onClick = clickAction,
             modifier = modifier
                 .sizeIn(
-                    minWidth = 48.dp,
-                    minHeight = 48.dp
+                    minWidth = 56.dp,
+                    minHeight = 56.dp
                 ),
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = if (it.isSecondary)
@@ -40,10 +44,18 @@ fun ButtonView(
             ),
             shape = Shapes.medium,
         ) {
-            it.textAttr.value?.let { text ->
+            it.icon?.let { image ->
+                Icon(
+                    imageVector = image,
+                    tint = colors.onSecondary,
+                    contentDescription = EMPTY_STRING
+                )
+            }
+            val text = it.text ?: it.textAttr.value?.let { stringResource(it) }
+            text?.let {
                 Text(
                     modifier = Modifier.padding(spacingSmall),
-                    text = stringResource(id = text),
+                    text = it,
                     color = colors.onSecondary,
                     fontSize = model.fontSize.value.sp,
                     textAlign = TextAlign.Center
@@ -58,6 +70,7 @@ fun ButtonView(
 fun PreviewButton() {
     val model = ButtonModel(
         fontSize = mutableStateOf(18f),
+        icon = Icons.Default.Check,
         textAttr = mutableStateOf(R.string.app_start),
         isSecondary = true
     )
