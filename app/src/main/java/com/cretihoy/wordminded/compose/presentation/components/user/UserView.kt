69e3405279d57029f46c29ffd4eac440d61db5f3
ellie <sergzhio238@gmail.com>
2022-12-05 14:31:51 +0100

    add users screen, view model and view

---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/user/UserView.kt
@@ -0,0 +1,50 @@
+package com.cretihoy.wordminded.compose.presentation.components.user
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cretihoy.wordminded.R
+import com.cretihoy.wordminded.compose.presentation.components.SpacerView
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonModel
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
+
+@Composable
+fun UserView(
+    model: UserModel,
+    removeAction: (UserModel) -> Unit = {},
+    editAction: (UserModel) -> Unit = {}
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        ButtonView(
+            modifier = Modifier.weight(1f),
+            model = model.nameButton,
+            clickAction = { editAction.invoke(model) }
+        )
+        SpacerView()
+        ButtonView(
+            model = model.removeButton,
+            clickAction = { removeAction.invoke(model) }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewUserView() {
+    val model = UserModel(
+        id = 123,
+        score = 0,
+        nameButton = ButtonModel(
+            fontSize = mutableStateOf(18f),
+            text = "Sashka"
+        ),
+        removeButton = ButtonModel(
+            fontSize = mutableStateOf(18f),
+            textAttr = mutableStateOf(R.string.plus)
+        )
+    )
+    UserView(model)
+}
---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/users/UsersScreen.kt
@@ -0,0 +1,83 @@
+package com.cretihoy.wordminded.compose.presentation.screen.users
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.cretihoy.wordminded.compose.presentation.components.SpacerView
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
+import com.cretihoy.wordminded.compose.presentation.components.dialog.DialogView
+import com.cretihoy.wordminded.compose.presentation.components.input.InputView
+import com.cretihoy.wordminded.compose.presentation.components.text.TextView
+import com.cretihoy.wordminded.compose.presentation.components.user.UserView
+import com.cretihoy.wordminded.compose.presentation.theme.spacingLarge
+
+@Composable
+fun UsersScreen(
+    viewModel: UsersViewModel
+) {
+    if (viewModel.users.isEmpty()) {
+        viewModel.loadUsers()
+    }
+    Box {
+        LazyColumn(
+            verticalArrangement = Arrangement.Bottom,
+            contentPadding = PaddingValues(spacingLarge),
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            item {
+                TextView(viewModel.titleModel, modifier = Modifier.fillMaxWidth())
+                SpacerView()
+            }
+            items(viewModel.users) {
+                UserView(
+                    model = it,
+                    editAction = { model ->
+                        viewModel.onEditClicked(model)
+                    },
+                    removeAction = { model ->
+                        viewModel.isDialogShown.value = true
+                        viewModel.currentUser = model
+                    }
+                )
+                SpacerView()
+            }
+            item {
+                ButtonView(
+                    viewModel.newUserButtonModel,
+                    modifier = Modifier.fillMaxWidth(),
+                    clickAction = {
+                        viewModel.isShownAdding.value = true
+                    })
+            }
+        }
+        viewModel.editInputModel.value?.let { model ->
+            InputView(
+                isShown = viewModel.isShownEdit,
+                model = model,
+                action = { name ->
+                    viewModel.onEditFinished(name)
+                }
+            )
+        }
+        InputView(
+            isShown = viewModel.isShownAdding,
+            model = viewModel.addingInputModel,
+            action = { name ->
+                viewModel.addUser(name)
+            }
+        )
+        DialogView(
+            viewModel.isDialogShown,
+            model = viewModel.dialogModel,
+            leftClick = {
+                viewModel.isDialogShown.value = false
+            }, rightClick = {
+                viewModel.onRemoveClicked()
+
+            }
+        )
+    }
+}
