c00b7637c80c9423b8dcd7f951f9fc0765ff21d9
ellie <sergzhio238@gmail.com>
2022-12-05 20:44:00 +0100

    add weight for text

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/counter/CounterView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/counter/CounterView.kt
@@ -6,11 +6,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.cretihoy.wordminded.R
 import com.cretihoy.wordminded.compose.presentation.components.SpacerView
+import com.cretihoy.wordminded.compose.presentation.components.button.ButtonModel
 import com.cretihoy.wordminded.compose.presentation.components.button.ButtonView
 import com.cretihoy.wordminded.compose.presentation.theme.spacingSmall
 
@@ -26,6 +30,7 @@ fun CounterView(
         model?.let { model ->
             model.textAttr?.let { text ->
                 Text(
+                    modifier = Modifier.weight(1f),
                     text = stringResource(id = text),
                     color = colors.onSecondary,
                     fontSize = model.fontSize.value.sp
@@ -52,4 +57,23 @@ fun CounterView(
             }
         }
     }
+}
+
+@Preview(widthDp = 250)
+@Composable
+fun PreviewCounterView() {
+    val fontsize = 28f
+    val model = CounterModel(
+        fontSize = mutableStateOf(fontsize),
+        textAttr = R.string.app_name,
+        leftButton = ButtonModel(
+            fontSize = mutableStateOf(fontsize),
+            text = "-"
+        ),
+        rightButton = ButtonModel(
+            fontSize = mutableStateOf(fontsize),
+            text = "+"
+        ),
+    )
+    CounterView(model)
 }
