4d033ba6cdd9bd8fc7d5a03af2aa68ee1e607e32
ellie <sergzhio238@gmail.com>
2022-12-06 19:56:43 +0100

    replace code in game screen

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameScreen.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/game/GameScreen.kt
@@ -1,22 +1,49 @@
 package com.cretihoy.wordminded.compose.presentation.screen.game
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import com.cretihoy.wordminded.compose.presentation.components.RotateScreen
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import com.cretihoy.wordminded.compose.presentation.components.SpacerView
+import com.cretihoy.wordminded.compose.presentation.components.gameside.GameSideView
+import com.cretihoy.wordminded.compose.presentation.components.rotate.RotateView
+import com.cretihoy.wordminded.compose.presentation.screen.end.EndGameScreen
+import com.cretihoy.wordminded.compose.presentation.theme.spacingLarge
 
 @Composable
 fun GameScreen(
     viewModel: GameViewModel
 ) {
-    RotateScreen(
-        horizontal = {
-            GameHorizontalScreen(
-                viewModel = viewModel
-            )
-        },
-        vertical = {
-            GameVerticalScreen(
-                viewModel = viewModel
-            )
+    Column(
+        modifier = Modifier
+            .padding(spacingLarge)
+            .fillMaxSize()
+    ) {
+        if (viewModel.task?.value != null && viewModel.letter?.value != null) {
+            RotateView(
+                header = {
+                    GameSideView(viewModel.gameSide.value,
+                        Modifier
+                            .weight(1f)
+                            .rotate(180f),
+                        actionLetter = { viewModel.onClick() },
+                        actionTask = { viewModel.onClick() }
+                    )
+                },
+                content = {
+                    SpacerView()
+                    GameSideView(
+                        viewModel.gameSide.value, Modifier.weight(1f),
+                        actionLetter = { viewModel.onClick() },
+                        actionTask = { viewModel.onClick() }
+                    )
+                })
+        } else {
+            EndGameScreen(viewModel.endGameModel) {
+                viewModel.newGame()
+            }
         }
-    )
+    }
 }
