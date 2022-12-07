943818f15092c8b2956ff13cf8b10e7808a87424
ellie <sergzhio238@gmail.com>
2022-12-07 13:51:05 +0100

    add clickable in row

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/switch/SwitchView.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/switch/SwitchView.kt
@@ -1,5 +1,6 @@
 package com.cretihoy.wordminded.compose.presentation.components.switch
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme.colors
@@ -9,11 +10,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.cretihoy.wordminded.R
 import com.cretihoy.wordminded.compose.presentation.components.SpacerView
+import com.cretihoy.wordminded.compose.presentation.theme.Shapes
 
 @Composable
 fun SwitchView(
@@ -22,7 +25,10 @@ fun SwitchView(
 ) {
     model?.textAttr?.let {
         Row(
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier
+                .fillMaxWidth()
+                .clip(Shapes.medium)
+                .clickable { model.isChecked.value = !model.isChecked.value },
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
