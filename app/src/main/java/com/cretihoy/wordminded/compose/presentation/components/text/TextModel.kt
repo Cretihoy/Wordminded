00e16055e8013994df55349e8bfcfff6fbaf84ad
Ellie <ellie@google.com>
2022-12-13 14:53:36 +0100

    add value

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/text/TextModel.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/text/TextModel.kt
@@ -6,6 +6,7 @@ import com.cretihoy.wordminded.compose.presentation.components.image.ImageModel
 data class TextModel(
     val fontSize: MutableState<Float>,
     val text: String? = null,
+    val value: String? = null,
     val textAttr: Int? = null,
     val isTitle: Boolean = false,
     val image: ImageModel? = null
