701c3993ea627848645aa184499981095b095e4a
Ellie <ellie@google.com>
2022-12-13 15:40:36 +0100

    refactor menu view model

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/menu/MenuViewModel.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/menu/MenuViewModel.kt
@@ -2,18 +2,32 @@ package com.cretihoy.wordminded.compose.presentation.screen.menu
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cretihoy.wordminded.compose.presentation.screen.users.UserRepository
+import com.cretihoy.wordminded.data.Storage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MenuViewModel
 @Inject constructor(
-    private val factory: MenuModelFactory
+    private val factory: MenuModelFactory,
+    private val storage: Storage,
+    private val repository: UserRepository
 ) : ViewModel() {
 
-    val isShown = mutableStateOf(false)
+    val isInfinityGame = storage.isInfinityGame
+    val users = repository.users
+
+    val isUsersShown = mutableStateOf(false)
+    val isCounterShown = mutableStateOf(false)
     val imageModel by lazy { factory.getImageModel() }
     val startButtonModel by lazy { factory.getStartButtonModel() }
     val rulesButtonModel by lazy { factory.getRulesButtonModel() }
     val settingsButtonModel by lazy { factory.getSettingsButtonModel() }
+
+    fun loadUsers() {
+        viewModelScope.launch { repository.loadUsers() }
+    }
 }
