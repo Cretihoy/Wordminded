2b8ce0589573947921deade91b6c60822c2b0b2c
Ellie <ellie@google.com>
2022-12-13 15:33:41 +0100

    refactor repository, viewmodel, screen logic

--- app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/users/UserRepository.kt
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/screen/users/UserRepository.kt
@@ -1,25 +1,38 @@
 package com.cretihoy.wordminded.compose.presentation.screen.users
 
+import androidx.compose.runtime.mutableStateListOf
 import com.cretihoy.wordminded.compose.presentation.components.user.UserModel
 import com.cretihoy.wordminded.data.db.dao.UserDao
+import com.cretihoy.wordminded.extensions.replace
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
     private val database: UserDao,
-    private val mapper: UserMapper
+    private val mapper: UserMapper,
+    private val factory: UserModelFactory
 ) {
 
-    suspend fun getUsers(): List<UserModel> {
-        return database.getAll().map { mapper.map(it) }
+    val users = mutableStateListOf<UserModel>()
+
+    suspend fun loadUsers() {
+        if (users.isEmpty()) {
+            users.addAll(database.getAll().map { mapper.map(it) })
+        }
     }
 
-    suspend fun addUser(model: UserModel) {
-        val user = mapper.map(model)
-        database.add(user)
+    suspend fun addUser(name: String) {
+        val user = factory.getUserModel(name)
+        users.add(user)
+        database.add(mapper.map(user))
     }
 
-    suspend fun removeUser(model: UserModel) {
-        val user = mapper.map(model)
-        database.remove(user)
+    suspend fun removeUser(user: UserModel) {
+        users.remove(user)
+        database.remove(mapper.map(user))
+    }
+
+    suspend fun replaceUser(remove: UserModel, put: UserModel) {
+        users.replace(remove, put)
+        database.add(mapper.map(put))
     }
 }
