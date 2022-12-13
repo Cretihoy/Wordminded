1667434e61c26d8d64bf1e2ceebe3bc9a88d66e1
Ellie <ellie@google.com>
2022-12-13 15:42:00 +0100

    add user picker view & viewmodel

---
+++ app/src/main/java/com/cretihoy/wordminded/compose/presentation/components/users/UserPickerViewModel.kt
@@ -0,0 +1,51 @@
+package com.cretihoy.wordminded.compose.presentation.components.users
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cretihoy.wordminded.compose.presentation.components.user.UserModel
+import com.cretihoy.wordminded.compose.presentation.screen.users.UserRepository
+import com.cretihoy.wordminded.data.Storage
+import com.cretihoy.wordminded.extensions.replace
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+private const val MAX_PLAYERS = 2
+
+@HiltViewModel
+class UserPickerViewModel
+@Inject constructor(
+    private val repository: UserRepository,
+    private val factory: UserPickerFactory,
+    private val storage: Storage
+) : ViewModel() {
+
+    val users = repository.users
+
+    val nextButtonModel by lazy { factory.getNextButtonModel() }
+    val titleModel by lazy { factory.getTitleModel() }
+
+    fun loadUsers() {
+        viewModelScope.launch {
+            repository.loadUsers()
+        }
+    }
+
+    fun onUserClicked(user: UserModel) {
+        val isNotFullUsers = users.filter { it.nameButton.isSecondary }.size < MAX_PLAYERS
+        val isDeactivate = user.nameButton.isSecondary
+
+        if (isNotFullUsers || isDeactivate) {
+            val isSecondaryToggle = !user.nameButton.isSecondary
+            val newButton = user.nameButton.copy(isSecondary = isSecondaryToggle)
+            val newUser = user.copy(nameButton = newButton)
+            users.replace(user, newUser)
+        }
+    }
+
+    fun onNextClicked() {
+        val players = users.filter { it.nameButton.isSecondary }
+        storage.firstPlayer.value = players.firstOrNull()
+        storage.secondPlayer.value = players.lastOrNull()
+    }
+}
